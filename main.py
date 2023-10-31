385e193da7e9cb01cea2c389b1feeacddb525360
kishanbabuji <113962450+kishanbabuji@users.noreply.github.com>
2023-10-30 22:18:07 -0400

    Initial Commit
    Commit with main.py and podcast_summarizer.py

---
+++ main.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import glob
+import json
+from podcast_summarizer import save_transcript
+
+st.title("Podcast Summarizer")
+with st.expander("How To Use"):
+    st.markdown(
+    """
+    To use our Podcast Summarizer web app, follow these simple steps:
+    - **Retrieve Episode ID:** To get started, you'll need the Episode ID of the podcast episode you want to summarize. 
+                            If you have the ListenNotes API, you can easily obtain this ID by searching for the desired episode.
+    - **Input Episode ID:** Enter the Episode ID into the text box to the left and the web app will begin summarizing the content. Please allow a few minutes for the script to complete, especially for longer podcasts. 
+    - **Review and Share:** After the summarization process is complete, you can review the generated summary. The summary contains chapter titles and summaries as well as timestamps for easy reference. 
+
+    By following these steps, you can quickly and efficiently generate episode summaries for your favorite podcasts, saving you time and helping you to grasp the main takeaways from the content you love. Enjoy using our Podcast Summarizer web app!
+    """
+    )
+
+json_files = glob.glob('*.json')
+
+episode_id = st.sidebar.text_input("Enter A Valid Episode ID")
+button = st.sidebar.button("Download Episode summary", on_click=save_transcript, args=(episode_id,))
+
+def get_clean_time_MS(start_ms):
+    seconds = int((start_ms / 1000) % 60)
+    minutes = int((start_ms / (1000 * 60)) % 60)
+    hours = int((start_ms / (1000 * 60 * 60)) % 24)
+    if hours > 0:
+        start_t = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+    else:
+        start_t = f'{minutes:02d}:{seconds:02d}'
+        
+    return start_t
+
+def get_clean_time_S(start_s):
+    seconds = int(start_s % 60)
+    minutes = int((start_s / 60) % 60)
+    hours = int((start_s / (60 * 60)) % 24)
+    if hours > 0:
+        start_t = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+    else:
+        start_t = f'{minutes:02d}:{seconds:02d}'
+        
+    return start_t
+
+
+if button:
+    filename = episode_id + '_chapters.json'
+    print(filename)
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    chapters = data['chapters']
+    episode_title = data['episode_title']
+    thumbnail = data['thumbnail']
+    podcast_title = data['podcast_title']
+    audio = data['audio_url']
+
+    st.header(f"{podcast_title} - {episode_title}")
+    st.image(thumbnail, width=200)
+    st.markdown(f'#### {episode_title}')
+    if data['explicit_content']:
+        st.markdown(f'##### Explicit Content')
+    st.markdown(f'##### Episode Duration: ' + get_clean_time_S(data['episode_duration']))
+
+    for chp in chapters:
+        with st.expander(chp['gist'] + ' - ' + get_clean_time_MS(chp['start'])):
+            chp['summary']
---
+++ podcast_summarizer.py
@@ -0,0 +1,97 @@
+import requests
+import json
+import time
+from api_secrets import API_KEY_ASSEMBLYAI, API_KEY_LISTENNOTES
+import pprint
+import os
+
+os.environ['API_KEY_ASSEMBLYAI'] = st.secrets['API_KEY_ASSEMBLYAI']
+os.environ['API_KEY_LISTENNOTES'] = st.secrets['API_KEY_LISTENNOTES']
+
+transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
+headers_assemblyai = {
+    "authorization": os.environ['API_KEY_ASSEMBLYAI'],
+    "content-type": "application/json"
+}
+
+listennotes_episode_endpoint = 'https://listen-api.listennotes.com/api/v2/episodes'
+headers_listennotes = {
+  'X-ListenAPI-Key': os.environ['API_KEY_ASSEMBLYAI'],
+}
+
+# Function to get the url with podcast audio for transcribing as well as key information about the podcast
+def get_episode_audio_url(episode_id):
+    url = listennotes_episode_endpoint + '/' + episode_id
+    response = requests.request('GET', url, headers=headers_listennotes)
+
+    data = response.json()
+    # pprint.pprint(data)
+
+    episode_title = data['title']
+    thumbnail = data['thumbnail']
+    podcast_title = data['podcast']['title']
+    audio_url = data['audio']
+    explicit_content = data['explicit_content']
+    episode_duration = data['audio_length_sec']
+    return audio_url, thumbnail, podcast_title, episode_title, explicit_content, episode_duration
+
+# Function to transcribe the podcast using Assembly AI
+def transcribe(audio_url, auto_chapters):
+    transcript_request = {
+        'audio_url': audio_url,
+        'auto_chapters': auto_chapters
+    }
+
+    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers_assemblyai)
+    pprint.pprint(transcript_response.json())
+    return transcript_response.json()['id']
+
+
+def poll(transcript_id):
+    polling_endpoint = transcript_endpoint + '/' + transcript_id
+    polling_response = requests.get(polling_endpoint, headers=headers_assemblyai)
+    return polling_response.json()
+    
+
+#Function to report on the status of transcription as AssemblyAI is processing the data 
+def get_transcription_result_url(url, auto_chapters):
+    transcribe_id = transcribe(url, auto_chapters)
+    while True:
+        data = poll(transcribe_id)
+        if data['status'] == 'completed':
+            return data, None
+        elif data['status'] == 'error':
+            return data, data['error']
+
+        print("waiting for 60 seconds")
+        time.sleep(60)
+            
+# Function to save key podcast info, chapter summaries and transcript 
+def save_transcript(episode_id):
+    audio_url, thumbnail, podcast_title, episode_title, explicit_content, episode_duration = get_episode_audio_url(episode_id)
+    data, error = get_transcription_result_url(audio_url, auto_chapters=True)
+    if data:
+        filename = episode_id + '.txt'
+        with open(filename, 'w') as f:
+            f.write(data['text'])
+
+        chapters_filename = episode_id + '_chapters.json'
+        with open(chapters_filename, 'w') as f:
+            chapters = data['chapters']
+
+            data = {'chapters': chapters}
+            data['audio_url']=audio_url
+            data['thumbnail']=thumbnail
+            data['podcast_title']=podcast_title
+            data['episode_title']=episode_title
+            data['explicit_content'] = explicit_content
+            data['episode_duration'] = episode_duration
+            # for key, value in kwargs.items():
+            #     data[key] = value
+
+            json.dump(data, f, indent=4)
+            print('Transcript saved')
+            return True
+    elif error:
+        print("Error!!!", error)
+        return False
